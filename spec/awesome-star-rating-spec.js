b96b362300832c47c74f1baa09a9a8eca28457ab
umov-mauricio <msganderla@umov.me>
2018-03-14 17:21:54 -0300

    starting with tests

---
+++ spec/awesome-star-rating-spec.js
@@ -0,0 +1,19 @@
+
+
+describe("Rating Start core", function(){
+
+
+    beforeEach(function(){
+        var $container = $("<div id='rating'>")
+
+        $('body').append($container);
+
+        $container.rating();
+
+    })
+
+    it('should create container with 5 stars as default', function(){
+        expect($('.rating-container rating-option').length).toBe(5)
+    });
+
+});
