1f5220a9ba53bd5d721093a3c6ff51dd7a4ae480
umov-mauricio <msganderla@umov.me>
2018-03-15 17:14:02 -0300

    trying to create more tests

--- spec/awesome-star-rating-spec.js
+++ spec/awesome-star-rating-spec.js
@@ -7,11 +7,19 @@ describe("Rating Start core", function(){
         $('body').append($container);
 
         $container.rating();
-
     })
 
     it('should create container with 5 stars as default', function(){
         expect($('.rating-container .rating-option').length).toBe(5)
+    });
+
+    it('should colorize the star clicked', function(){
+        $('.rating-container .rating-option').eq(3).trigger('click');
+        expect($('.rating-container .filled').length).toBe(2)
+    });
+
+    afterEach(function() {
+        $('#multiselect').remove();
     });
 
 });
