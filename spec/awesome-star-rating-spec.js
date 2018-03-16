b2ea3753f506a2648cc589a22664a93d3149116f
umov-mauricio <msganderla@umov.me>
2018-03-16 11:29:33 -0300

    created test that when click should set the value of start clicked

--- spec/awesome-star-rating-spec.js
+++ spec/awesome-star-rating-spec.js
@@ -3,13 +3,14 @@ describe("Rating Start core", function(){
 
     beforeEach(function(){
         var $container = $("<div id='rating'></div>")
+        var $ratingValue = $('<span id="value"></span>')
 
-        $('body').append($container);
+        $('body').append($container, $ratingValue);
 
         $container.rating();
 
         $container.on('rating:rate','.rating-option', function(event, value,identifier, fillRating){
-            $("#value").html(value)
+            $ratingValue.html(value)
             fillRating()
         });
     })
@@ -23,6 +24,10 @@ describe("Rating Start core", function(){
         expect($('.rating-container .filled').length).toBe(2)
     });
 
+    it('should print the value of the star clicked', function(){
+        $('.rating-container .rating-option').eq(3).trigger('click');
+        expect($('#value').text()).toBe('2')
+    });
 
     afterEach(function() {
         $('#rating').remove();
